605c35099a8b96fe4521a883c9897cfdf075de16
kningtg <kningtg@gmail.com>
2022-10-04 04:13:24 +0800

    tmp commit Tue 04 Oct 2022 02:43:26 AM +08

--- zzbashrc.sh
+++ zzbashrc.sh
@@ -3,4 +3,4 @@ set -e
 
 ROOTDIR=${ZZROOT:-$HOME/app}
 export PATH="$ROOTDIR/bin:$ROOTDIR/include:$ROOTDIR/lib:$ROOTDIR/lib/pkgconfig:$ROOTDIR/lib/share/pkgconfig:$PATH"
-export LD_LIBRARY_PATH="$ROOTDIR/lib"\" >> $HOME/.bashrc
+export LD_LIBRARY_PATH="$ROOTDIR/lib"
